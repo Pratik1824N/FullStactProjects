b74cbf4d35bbbbd4f860a53cccfc15adfd10ac79
Pratik1824N <94686241+Pratik1824N@users.noreply.github.com>
2025-08-06 13:44:45 +0530

    Update ContactController.java

--- src/main/java/com/pratik/contactapp/controller/ContactController.java
+++ src/main/java/com/pratik/contactapp/controller/ContactController.java
@@ -1,1 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
+package com.example.demo.controller;
+
+import com.example.demo.model.Contact;
+import com.example.demo.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author HP
+ */
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class ContactController {
+    
+   @Autowired
+   private ContactService contactService;
+   
+   @PostMapping("/contact")
+   public String saveContact(@RequestBody Contact contact){
+       return contactService.saveContact(contact); 
+   }
+   
+
+}
