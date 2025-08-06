6dd0d0fbf683973aa1a8fbfd9bfc0763b482941d
Pratik1824N <94686241+Pratik1824N@users.noreply.github.com>
2025-08-06 13:25:01 +0530

    Create ContactFormApplication.java

---
+++ src/main/java/com/pratik/contactapp/ContactFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactFormAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactFormAppApplication.class, args);
+	}
+
+}
